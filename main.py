03f75de5171e86ee8d43f30bcd06d9d614e24ae4
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-06-27 13:36:41 +0300

    MASTER: first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+global_logic = ('Shashank Samant', ' Raj Radhakrishnan', 'Nitesh Banga', 'Scott Brubaker', 'Arya Barirani',
+                'Zaheer Allam', 'Jim Walsh', 'Amy Hanlon-Rodemich')
+
+print(global_logic)
+
+toshiba
