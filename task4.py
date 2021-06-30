47624dbedbee2a51b571c45921f5af151f2c5a73
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-06-30 20:59:10 +0300

    HW2: add new file task3.py

--- task4.py
+++ task4.py
@@ -1,19 +1,23 @@
-vip = {
-    "table 1": "vip1",
-    "table 2": "vip2",
-    "table 3": "vip3",
-    "table 4": "vip4",
-    "table 5": "vip6",
-}
+vip_stage = (
+    'vip guest 1',
+    'vip guest 2',
+    'vip guest 3',
+    'vip guest 4',
+    'vip guest 5',
+    'vip guest 6'
+)
 
 main_stage = {
-    "ticket 1": "human 1",
-    "ticket 2": "human 2",
-    "ticket 3": "human 3",
-    "ticket 4": "human 4",
-    "ticket5": "human 5",
-    "ticket6": "human 6"
+    "regular guest 1",
+    "regular guest 2",
+    "regular guest 3",
+    "regular guest 4",
+    "regular guest 5",
+    "regular guest 6"
 }
 
-print(vip)
-print(main_stage)
+for vip_guests in vip_stage:
+    print(vip_guests)
+
+for regular_guests in main_stage:
+    print(regular_guests)
