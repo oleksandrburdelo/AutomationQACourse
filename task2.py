8572aff0302fdfea901ff537ae02fec41cb0c0ab
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-07-01 20:39:21 +0300

    HW2: HomeTask ver 2.0

--- task2.py
+++ task2.py
@@ -4,8 +4,3 @@ bingo_blacklist = {'John Dow', 'Marta Dow', 'Richard Dow'}
 poker_blacklist = {'John Dow', 'Marta Wod', 'Richard Wod'}
 majong_blacklist = {'Dow Illya', 'John Dow', 'Dr. Willson'}
 print(bingo_blacklist.intersection(poker_blacklist, majong_blacklist))
-
-# Proper structure was choosen but take a look on another methods of sets.
-# you need to get all names which exists in all sets. For example
-# I have update your sets and John Dow noe in each set. So you need to use one
-# of the method of set to get john dow. Take a look on main methods of set.
--- task3.py
+++ task3.py
@@ -6,6 +6,3 @@ people = [vegans, meat_and_vegetable_lovers]
 for index in people:
     for index2 in index:
         print(index2)
-
-# Interesting solution but you have to create new one list of people who can
-# each vegetables. Tuple is not the best data type for this task.
