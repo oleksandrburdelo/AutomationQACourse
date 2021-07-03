fe73e420e52eb897ed5c8248a1ef060e9f562876
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-07-03 15:43:31 +0300

    HW3: task2

---
+++ task2.py
@@ -0,0 +1,26 @@
+#a = 2
+#a * 2
+#print(a)
+
+#a = 2
+#b = 2
+#c = a * b
+#print(c)
+
+a = 2
+b = 2
+c = a ** b
+print(c)
+
+#a = 2
+#b = 2
+#c = a / b
+#print(c)
+
+#a = 2
+#b = 2
+#c = a % b
+#print(c)
+
+#a / a
+#print(a)
