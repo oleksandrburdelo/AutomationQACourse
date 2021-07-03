b05c63629d8ca4ca6a1585341108c73c03256604
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-07-03 17:27:59 +0300

    HW3: task3

--- task2.py
+++ task2.py
@@ -7,10 +7,10 @@
 #c = a * b
 #print(c)
 
-a = 2
-b = 2
-c = a ** b
-print(c)
+#a = 2
+#b = 2
+#c = a ** b
+#print(c)
 
 #a = 2
 #b = 2
---
+++ task3.py
@@ -0,0 +1,8 @@
+friends = ["John", "Marta", "James"]
+enemies = ["John", "Johnatan", "Artur"]
+
+if 'Marta' in friends:
+    if 'Johnatan' not in friends:
+        print(f"{enemies} we are not the friends anymore")
+    print(f"{friends} we are the best friends")
+
