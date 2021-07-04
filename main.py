21cddd727bd39fa0f32e4d9f3a3b1068d1cfa527
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-04 14:39:10 +0300

    HW3: add alternative solutions and describe issues in comments
    * add description of issue in task 1 and examples of code with recomendations
    
    * add alternative solution in task 2
    
    * add description of issue in task 3 and extend list of friends to show the problems with implemented solution

--- main.py
+++
@@ -1,6 +0,0 @@
-global_logic = ('Shashank Samant', ' Raj Radhakrishnan', 'Nitesh Banga', 'Scott Brubaker', 'Arya Barirani',
-                'Zaheer Allam', 'Jim Walsh', 'Amy Hanlon-Rodemich')
-
-print(global_logic)
-
-toshiba
--- task2.py
+++ task2.py
@@ -25,3 +25,8 @@
 
 #a / a
 #print(a)
+
+# Good. I have not clarify which operators ;you should use.
+# But alternative solution for multiplying and dividing by 2 was bit operator
+# b = a << 1  # multiply on 2
+# e = a >> 1  # divide on 2
