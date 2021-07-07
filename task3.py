4151c865434caed1171b53d5069727b1a0304894
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-07-07 18:46:05 +0300

    HW4: new

--- task3.py
+++
@@ -1,8 +0,0 @@
-friends = ["John", "Marta", "James"]
-enemies = ["John", "Johnatan", "Artur"]
-
-if 'Marta' in friends:
-    if 'Johnatan' not in friends:
-        print(f"{enemies} we are not the friends anymore")
-    print(f"{friends} we are the best friends")
-
